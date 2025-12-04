20013a6d913775e21f3081b644384160d56f0a51
unknown <salah-eddine-555>
2025-12-04 20:07:01 +0100

    mainuler les requets existe dans simplon

--- manupilatioDB.sql
+++ manupilatioDB.sql
@@ -16,3 +16,21 @@ SELECT * FROM Sponsors WHERE montant >= '3500';
 SELECT count(*) FROM Evenements;
 
 SELECT AVG(budget) AS moyenne FROM Evenements;
+
+##7
+SELECT AVG(budget) AS moyenne FROM Evenements;
+
+
+## 8
+SELECT nomEven , datee ,nomEntre from Evenements e
+JOIN Sponsors s ON
+  s.idSponsor IN (
+  SELECT es.id_sponsor from EvenementSponsor es
+  where es.id_sponsor = s.idSponsor
+  )
+  where nomEntre = 'TechnoPlus';
+  
+##9
+SELECT * FROM Evenements where lieu = 'casablanca';
+## 10
+SELECT * FROM Evenements order by datee DESC LIMIT 5;
